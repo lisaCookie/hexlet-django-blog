a64f12e734b112ce8d1e76262ebe0925838bb0a4
lisa <lisavetabelova@gmail.com>
2025-09-27 02:19:25 +0300

    modified:   .gitignore         modified:   hexlet_django_blog/article/urls.py         modified:   hexlet_django_blog/settings.py         modified:   pyproject.toml         modified:   uv.lock

--- hexlet-django-blog/hexlet_django_blog/article/urls.py
+++ hexlet-django-blog/hexlet_django_blog/article/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-
-from hexlet_django_blog.article import views
 from .views import IndexView, HomeView
 
 urlpatterns = [
--- hexlet-django-blog/hexlet_django_blog/settings.py
+++ hexlet-django-blog/hexlet_django_blog/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-og#!fsv37=e&$-gm*k$af3g4!b6d%@fzr6yt^^%7a#w#si)@3#'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
